51ac9292433828aa3fcd580c08852b1ce625a677
Wirelli <ironfroststudios@gmail.com>
2024-12-17 11:55:40 -0500

    Uploading files

---
+++ src/main/java/com/wirelli/tutorialmod/item/ModItems.java
@@ -0,0 +1,28 @@
+package com.wirelli.tutorialmod.item;
+
+import com.wirelli.tutorialmod.TutorialMod;
+import com.wirelli.tutorialmod.item.custom.ChiselItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = // Used to register THINGS, items, blocks, creative tabs, etc.
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
